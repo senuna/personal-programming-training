bd08f42a3178ea7a4c3a61e8751c9d4d21856bb3
senuna <i.senuna@gmail.com>
2016-11-03 00:05:28 +0100

    training with Section 5 - finishing

--- CompleteJavaDeveloper/Section05-Expressions-Statements-Code blocks-Methods/src/Main.java
+++ CompleteJavaDeveloper/Section05-Expressions-Statements-Code blocks-Methods/src/Main.java
@@ -82,6 +82,42 @@ public class Main {
         calculateScore();
         //endregion
 
+        //region part4 challenge
+        //***************************************************
+        // Part4 calcFeetAndInchesToCentimeters
+        //***************************************************
+        // Create a method called calcFeetAndInchesToCentimeters
+        // It needs to have two parameters.
+        // feet is the first parameter, inches is the 2nd parameter
+        //
+        // You should validate that the first parameter feet is >= 0
+        // You should validate that the 2nd parameter inches is >=0 and <=12
+        // return -1 from the method if either of the above is not true
+        //
+        // If the parameters are valid, then calculate how many centimetres
+        // comprise the feet and inches passed to this method and return
+        // that value.
+        //
+        // Create a 2nd method of the same name but with only one parameter
+        // inches is the parameter
+        // validate that its >=0
+        // return -1 if it is not true
+        // But if its valid, then calculate how many feet are in the inches
+        // and then here is the tricky part
+        // call the other overloaded method passing the correct feet and inches
+        // calculated so that it can calculate correctly.
+        // hints: Use double for your number datatypes is probably a good idea
+        // 1 inch = 2.54cm  and one foot = 12 inches
+        // use the link I give you to confirm your code is calculating correctly.
+        // Calling another overloaded method just requires you to use the
+        // right number of parameters.
+
+        System.out.println("Part 4 - chellange");
+        calcFeetAndInchesToCentimeters(13,25);
+        calcFeetAndInchesToCentimeters(100);
+
+        //endregion
+
     }
     // functions
     //region Part 1
@@ -131,6 +167,33 @@ public class Main {
 
         System.out.println("No player, no score ;-)");
         return 0;
+    }
+    //endregion
+
+    //region Part 4
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if((feet < 0) || ((inches < 0) || (inches >12))){
+            System.out.println("Nope!");
+            return -1;
+        }
+
+        // double cm;
+        double cm = (feet * 12 ) * 2.54; //calculating feets
+        cm += inches * 2.54;
+
+        System.out.println(feet + " feet and " + inches +" inches = " + cm + " centimeters");
+        return cm;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+      if(inches < 0){
+          return -1;
+      }
+
+      double feet = (int) inches / 12;
+      double remainingInch = (int) inches % 12;
+        System.out.println(inches + "inches = " + feet + " feets "+ remainingInch + "inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInch);
     }
     //endregion
 
