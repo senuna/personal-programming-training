a1a846b5a698f16accfd45a108f17746aaeffa0e
senuna <i.senuna@gmail.com>
2016-11-05 00:40:54 +0100

    Section 6 - control flow statements - finishing

--- CompleteJavaDeveloper/Section06-Control-Flow-Statements/src/ForStatement_Challenge.java
+++ CompleteJavaDeveloper/Section06-Control-Flow-Statements/src/ForStatement_Challenge.java
@@ -3,6 +3,78 @@
  */
 public class ForStatement_Challenge {
     public static void main(String[] args) {
-        System.out.println("cosiek kolejny");
+
+        //System.out.println("10000 with 2% is " + calculateInterest(10000.0, 2.0) );
+
+        // using the for statement, call the calculateInterest method with
+        // the amount of 10000 with an interestRate of 2,3,4,5,6,7, and 8
+        // and print the results to the console window.
+
+        double interestRate = 8;
+        for ( int i = 2; i <= interestRate ;i++ ){
+            System.out.println("10000 with " + i + "% interest " + " is " + calculateInterest(10000.0, i ));
+        }
+
+        // How would you modify the for loop above to do the same thing as
+        // shown but to start from 8% and work back to 2%
+
+        interestRate = 8;
+        for (int i = (int) interestRate; i >= 2 ;i--){
+            System.out.println("10000 with " + i + "% interest " + " is " + String.format("%.2f",calculateInterest(10000.0, i )));
+        }
+
+        // Create a for statement using any range of numbers
+        // Determine if the number is a prime number using the isPrime method
+        // if it is a prime number, print it out AND increment a count of the
+        // number of prime numbers found
+        // if that count is 3 exit the for loop
+        // hint:  Use the break; statement to exit
+
+        int count = 0;
+        int start = 10;
+        int end = 50;
+
+        for (int i = start; i <= end; i++ ){
+            if (isPrime(i)){
+                System.out.println("Number " + i + " is prime number.");
+                count++;
+
+                if (count == 3){
+                    System.out.println("It's enough. Between given " + start + " and " + end + " I just listed 3 prime numbers");
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static boolean isPrime (int n){
+
+        if (n == 1){
+            return false;
+        }
+
+        for (int i=2; 2*i <= n; i++){
+            // checking with and compare how many times it loop
+            // i <= (long) Math.sqrt(n)
+
+            // if there will be any number that divides given one it returns false
+            //senuna: Consider that if 2 divides some integer n, then (n/2) divides n as well.
+            // This tells us we don't have to try out all integers from 2 to n.
+            // and 2*i = n is more safe than i = n/2
+
+            //System.out.println("Loop nr " + i);
+
+            if (n % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static double calculateInterest (double amount, double interestRate){
+        double result = amount * (interestRate/100);
+        return result;
     }
 }
--- CompleteJavaDeveloper/Section06-Control-Flow-Statements/src/WhileDoWhile_Challenge.java
+++ CompleteJavaDeveloper/Section06-Control-Flow-Statements/src/WhileDoWhile_Challenge.java
@@ -1,8 +1,92 @@
 /**
- * Created by senuna on 03.11.16.
+ * Created by senuna on 04.11.16.
  */
 public class WhileDoWhile_Challenge {
     public static void main(String[] args) {
-        System.out.println("aham!");
+        int a = 1;
+        while (a !=6 ){
+            System.out.println("aham! " + a);
+            a++;
+        }
+
+        a = 1;
+        while (true){
+            if(a ==6){
+                break;
+            }
+            System.out.println("cos " + a);
+            a++;
+        }
+
+        //senuna: be carrefull!
+        //a = 6;
+
+        a = 3;
+        do {
+            System.out.println("a value is" + a);
+            a++;
+        } while (a != 6);
+
+        // Modify the while code above
+        // Make it also record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end, display the total number of even numbers found
+
+        // Create a method called isEvenNumber that takes a parameter of type int
+        // Its purpose is to determine if the argument passed to the method is
+        // an even number or not.
+        // return true if an even number, otherwise return false;
+
+        int n = 5;
+        int end = 20;
+
+        while (n <= end){
+            if (isEven(n)){
+                System.out.println(n +" is an even number.");
+                n++;
+            } else {
+                n++;
+                continue;
+            }
+
+        }
+
+        // Modify the while code above
+        // Make it also record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end, display the total number of even numbers found
+
+        n = 25;
+        end = 60;
+        int count = 0;
+
+        while (n <= end){
+            if (isEven(n)){
+                System.out.println(n +" is an even number.");
+                n++;
+                count++;
+                if (count == 5){
+                    System.out.println("I've found 5 even numbers. Exit");
+                    break;
+                }
+
+            } else {
+                n++;
+                continue;
+            }
+
+        }
+        System.out.println("I've found " + count + " even numbers");
+
+
+
+    }
+
+    public static boolean isEven(int n){
+        if ((n%2) == 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
