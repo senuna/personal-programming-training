5ef687b8241228690288301be33e47e254d22277
senuna <i.senuna@gmail.com>
2016-11-11 23:33:10 +0100

    Section 08 - composition 04

--- CompleteJavaDeveloper/Section08-OOP2-Composition-Encapsulation-Polymorphism/src/eu/senuna/oop2/compositionRoom/Main.java
+++ CompleteJavaDeveloper/Section08-OOP2-Composition-Encapsulation-Polymorphism/src/eu/senuna/oop2/compositionRoom/Main.java
@@ -14,5 +14,21 @@ public class Main {
         // then add at least one method to hide the object e.g. not using a getter
         // but to access the object used in composition within the main class
         // like you saw in this video.
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("North");
+        Wall wall4 = new Wall("South");
+
+        Ceiling ceiling = new Ceiling(250, 55);
+
+        Bed bed = new Bed("Ornamental", 2, 1, 2, 1);
+        Lamp lamp = new Lamp("Designed", false, 53);
+
+        Bedroom inesBedroom = new Bedroom("Iness", wall1,wall2, wall3, wall4, ceiling, bed, lamp);
+
+        inesBedroom.makeBed();
+        inesBedroom.getLamp().turnOn();
+
     }
 }
