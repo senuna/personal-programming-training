751d113c7f8ca432e1d9d255b5b68b9fd9e8549c
senuna <i.senuna@gmail.com>
2016-11-14 23:49:47 +0100

    Section 09-01 - Arrays and ArrayLists

--- CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/Main.java
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/Main.java
@@ -1,11 +1,16 @@
 package eu.senuna;
 
+import java.util.Scanner;
+
 /**
  * Created by senuna on 14.11.16.
  */
 
 //useful in arrays: myIntArray.length
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         //region intro to arrays
@@ -29,12 +34,39 @@ public class Main {
         //endregion
 
         //region
+        int[] myIntegeres = getIntegers();
+        for (int i = 0; i < myIntegeres.length; i++ ){
+            System.out.println("Element " + i + " was given value " + myIntegeres[i]);
+        }
 
+        System.out.println("Average is " + getAverage(myIntegeres));
         //endregion
 
-
-
     }
+
+    public static int[] getIntegers(){
+        System.out.println("enter number for indexes of array");
+        int number = scanner.nextInt();
+        System.out.println("Your chosen number is " + number);
+
+        int[] values = new int[number];
+
+        for(int i = 0 ; i < values.length; i++){
+            System.out.println("enter number for fill array index " + i );
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for (int i=0; i <array.length; i++){
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
+    }
+
 
     public static void printArray(int[] array){
         for (int i=0; i<array.length; i++){
