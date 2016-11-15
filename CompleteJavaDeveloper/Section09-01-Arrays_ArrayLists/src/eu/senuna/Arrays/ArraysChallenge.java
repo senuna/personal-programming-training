26c2dd9be12d798621f5e45a7916ee0b059d5356
senuna <i.senuna@gmail.com>
2016-11-15 23:58:30 +0100

    Section 09-02 - Arrays and ArrayLists

--- CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/Arrays/ArraysChallenge.java
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/Arrays/ArraysChallenge.java
@@ -1,5 +1,7 @@
 package eu.senuna.Arrays;
 
+import java.util.Scanner;
+
 /**
  * Created by senuna on 14.11.16.
  */
@@ -15,7 +17,38 @@ public class ArraysChallenge {
     // and sortIntegers should sort the array and return a new array containing the sorted numbers
     // you will have to figure out how to copy the array elements from the passed array into a new
     // array and sort them and return the new sorted array.
+
+    //senuna: it's nice and usefull :)
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        System.out.println("enter number for indexes of array \r");
+        int number = scanner.nextInt();
+
+        int[] myIntegers = getIntegers(number);
+
+
     }
+
+    public static int[] getIntegers(int size){
+        int[] array = new int[size];
+        System.out.println("Please provide " + size + " integer values: \r");
+        for (int i=0; i<array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+        return  array;
+    }
+
+    public static void printArray(int[] array){
+        for (int i=0; i<array.length; i++){
+            System.out.println("Indeks " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+        
+    }
+
 }
