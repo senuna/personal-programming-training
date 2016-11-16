6070174cf8b9da096f71397ab516c5d334ff690f
senuna <i.senuna@gmail.com>
2016-11-16 23:01:45 +0100

    Section 09-05 - ArrayLists

---
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ListsAndArrayLists/ArrayListIntro.java
@@ -0,0 +1,7 @@
+package eu.senuna.ListsAndArrayLists;
+
+/**
+ * Created by senuna on 16.11.16.
+ */
+public class ArrayListIntro {
+}
---
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ListsAndArrayLists/GroceryList.java
@@ -0,0 +1,36 @@
+package eu.senuna.ListsAndArrayLists;
+
+import java.util.ArrayList;
+
+/**
+ * Created by senuna on 16.11.16.
+ */
+public class GroceryList {
+    //list of objects
+    private ArrayList<String> groceryList = new ArrayList<String>();
+    //it's a method - so the empty Constructor will be called
+    private static int[] myNumbers = new int[50];
+
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your list");
+        for(int i=0; i<groceryList.size(); i++){
+            System.out.println((i+1) + " " + groceryList.get(i)); //we call a method for getting items
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("You just changed item number: " + (position+1) + " from your list");
+    }
+
+    public void removeGroceryItem(int position){
+        //TODO: change for positions
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+}
