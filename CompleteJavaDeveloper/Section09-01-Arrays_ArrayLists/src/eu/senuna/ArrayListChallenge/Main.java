031de1564c6c0aa84b5d34f0a82dc36fa82dffea
senuna <i.senuna@gmail.com>
2016-11-23 01:31:02 +0100

    Section 09-12 - ArrayLists - MobilePhoneChallenge

--- CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ArrayListChallenge/Main.java
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ArrayListChallenge/Main.java
@@ -30,17 +30,87 @@ public class Main {
         startPhone();
         printActions();
         while(!quit){
+            System.out.println("\nEnter action: (6 for print available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("\n Shutting down");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+
+            }
 
         }
 
     }
 
+    public static void addNewContact(){
+        System.out.println("Enter new name for your contact: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        Contact newContact = Contact.createRecord(name, phoneNumber); //easier way... becouse of static
+        if(mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added: " + name + " , phone = " + phoneNumber);
+        } else {
+            System.out.println("Contact "+ name + " already on list. Has to be uniuqe.");
+        }
+    }
+
+    private static void updateContact(){
+        System.out.println("Enter contact name existing in list: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+        if(existingContactRecord == null){
+            System.out.println("Contact not found");
+            return null;
+        }
+        System.out.println("Enter new contact name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Enter new contact phone number: ");
+        String newPhoneNumber = scanner.nextLine();
+        Contact newContact = Contact.createRecord(newName, newPhoneNumber);
+        if(mobilePhone.updateContact(existingContactRecord, newContact)){
+            System.out.println("Udadated status: ok");
+        } else {
+            System.out.println("Updating status: error by ");
+        }
+    }
+
     private static void startPhone(){
+        System.out.println("Starting the mobile ");
 
     }
 
     private static void printActions(){
-
+        System.out.println("\n available actions: \n press");
+        System.out.println("0 - shutdown \n" +
+                        "1 - print contacts \n" +
+        "2 - add new contact \n"+
+        "3 - update \n"+
+        "4 - remove contact\n"+
+        "5 - query if an contact exist\n"+
+        "6 - print available actions");
+        System.out.println("chooce your action: ");
     }
 
 }
--- CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ArrayListChallenge/MobilePhone.java
+++ CompleteJavaDeveloper/Section09-01-Arrays_ArrayLists/src/eu/senuna/ArrayListChallenge/MobilePhone.java
@@ -67,4 +67,22 @@ public class MobilePhone {
         }
         return null;
     }
+
+    public Contact queryContact(String name){
+        int position = findContact(name);
+        if(position >= 0) {
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+
+    public void printContacts(){
+        System.out.println("Contact list: ");
+        for(int i=0; i<this.myContacts.size(); i++){
+            System.out.println((i+1)+"."+
+            this.myContacts.get(i).getName() + " - " +
+            this.myContacts.get(i).getPhoneNumber());
+        }
+    }
 }
