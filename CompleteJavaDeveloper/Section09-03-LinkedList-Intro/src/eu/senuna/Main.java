701454e9f8d9f751ac2e04c3f19d31070eb8fd4b
senuna <i.senuna@gmail.com>
2016-12-02 23:58:38 +0000

    Section 09-22 - Linked List

--- CompleteJavaDeveloper/Section09-03-LinkedList-Intro/src/eu/senuna/Main.java
+++ CompleteJavaDeveloper/Section09-03-LinkedList-Intro/src/eu/senuna/Main.java
@@ -1,5 +1,7 @@
 package eu.senuna;
 
+import java.util.ArrayList;
+
 /**
  * Created by senuna on 30.11.16.
  */
@@ -8,6 +10,25 @@ public class Main {
         Customer customer = new Customer("Jadzka", 25.26);
         Customer nextCustomer;
         nextCustomer = customer;
-        
+        nextCustomer.setBalance(12.65);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance() );
+    // important!! it uses pointers
+
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i=0; i<intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for (int i=0; i<intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
     }
 }
