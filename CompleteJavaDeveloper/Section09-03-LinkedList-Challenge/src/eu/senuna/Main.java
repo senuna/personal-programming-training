f6b381d59fdaf872b8270a05e36287216715273c
senuna <i.senuna@gmail.com>
2016-12-07 00:01:09 +0000

    Section 09-27 - Linked List - Challenge 3 finish

--- CompleteJavaDeveloper/Section09-03-LinkedList-Challenge/src/eu/senuna/Main.java
+++ CompleteJavaDeveloper/Section09-03-LinkedList-Challenge/src/eu/senuna/Main.java
@@ -16,10 +16,7 @@ package eu.senuna;
 // As an optional extra, provide an option to remove the current song from the playlist
 // (hint: listiterator.remove()
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by senuna on 04.12.16.
@@ -78,6 +75,7 @@ public class Main {
             return;
         } else {
             System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
         }
 
         while (!quit){
@@ -112,6 +110,21 @@ public class Main {
                     }
                     break;
                 case 3:
+                    if(forward){
+                        if(listIterator.hasPrevious()){
+                            System.out.println("Now replay " + listIterator.previous().toString() );
+                            forward = false;
+                        } else {
+                            System.out.println("It's the beginning of the list");
+                        }
+                    } else {
+                        if (listIterator.hasNext()){
+                            System.out.println("Now replaying "+ listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reach the end of the list");
+                        }
+                    }
                     break;
                 case 4:
                     printList(playList);
@@ -119,7 +132,38 @@ public class Main {
                 case 5:
                     printMenu();
                     break;
+                case 6:
+                    if(playList.size() >0 ){
+                        listIterator.remove(); //will couse an error if trying to remove second option
+                        if(listIterator.hasNext()){
+                            System.out.println("Now playing " + listIterator.next().toString());
+                        } else if(listIterator.hasPrevious()){
+                            System.out.println("Now playing " + listIterator.previous().toString());
+                        }
+                    }
+                    break;
             }
         }
+    }
+
+    private static void printMenu(){
+        System.out.println("Available options:\n press: ");
+        System.out.println(" " +
+                        "0 to quit \n"+
+                "1 - play next\n" +
+                "2 - play previous \n" +
+                "3 - replay current playing\n" +
+                "4 - show songs in the playlist\n" +
+                "5 - print options \n" +
+                "6 - remove current playing song \n");
+    }
+
+    private static void printList(LinkedList<Song> playList){
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("----------");
+        while ((iterator.hasNext())){
+            System.out.println(iterator.next());
+        }
+        System.out.println("----------");
     }
 }
